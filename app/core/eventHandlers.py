2c9dc72e4523c44c83f1c2aa374403786cdbb0bf
Ashwin Hegde <ashwin.hegde3@gmail.com>
2022-08-01 23:29:34 +0530

    feat(event_handler): add comments

--- app/core/eventHandlers.py
+++ app/core/eventHandlers.py
@@ -9,11 +9,17 @@ def _shutdown_model(app: FastAPI) -> None:
   pass
 
 def start_app_handler(app: FastAPI) -> Callable:
+  """
+  Startup event handler
+  """
   def startup() -> None:
     _startup_model(app)
   return startup
 
 def shutdown_app_handler(app: FastAPI) -> Callable:
+  """
+  Shutdown event handler
+  """
   def shutdown() -> None:
     _shutdown_model(app)
   return shutdown

786eba302fba5edd94bf45a761430a9dd7f5729a
Ashwin Hegde <ashwin.hegde3@gmail.com>
2022-08-02 22:04:56 +0530

    feat(cache): add lru cache

--- app/core/commonHandlers.py
+++ app/core/commonHandlers.py
@@ -1,9 +1,11 @@
 from config import Settings
-# from functools import lru_cache
+from functools import lru_cache
 
-# @lru_cache()
+@lru_cache()
 def get_settings():
   """
   Return Settings class which holds application's metadata info
+  @lru_cache() Decorates to return the same value that was returned the first time,
+  instead of computing it again, executing the code of the function every time.
   """
   return Settings()

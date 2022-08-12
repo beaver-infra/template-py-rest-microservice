13844646a2f04de0adc97bd73842a535fc06b1d6
Ashwin Hegde <ashwin.hegde3@gmail.com>
2022-08-13 00:13:31 +0530

    refactor(app): simply

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi_versioning import VersionedFastAPI
-# from app.endpoints.router import api_router
 from app.api import api_router
 from app.config import Settings
 
@@ -18,45 +17,34 @@ from app.config import Settings
 # This will get the root logger since no logger in the configuration has this name.
 # logger = logging.getLogger(__name__)
 
-def get_app() -> FastAPI:
-  """
-  Create FastAPI app instance and return with default configured settings
-  """
-  setting = Settings()
-  fast_app = FastAPI(
-    title=setting.title,
-    description=setting.description,
-    contact=setting.contact,
-    debug=setting.is_debug_mode(),
-  )
+# Create FastAPI app instance and return with default configured settings
+setting = Settings()
+app = FastAPI(
+  title=setting.title,
+  description=setting.description,
+  contact=setting.contact,
+  debug=setting.is_debug_mode(),
+)
 
-  # Allow cross origin resource sharing with default configurations
-  # Refer https://fastapi.tiangolo.com/tutorial/cors/
-  fast_app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-  )
+# Allow cross origin resource sharing with default configurations
+# Refer https://fastapi.tiangolo.com/tutorial/cors/
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
+)
 
-  # Compress response for any request that includes gzip in the accept-encoding header
-  # Refer https://fastapi.tiangolo.com/advanced/middleware/#gzipmiddleware
-  fast_app.add_middleware(GZipMiddleware)
+# Compress response for any request that includes gzip in the accept-encoding header
+# Refer https://fastapi.tiangolo.com/advanced/middleware/#gzipmiddleware
+app.add_middleware(GZipMiddleware)
 
-  # Reference to all the microservice routes including special routes like info and health
-  fast_app.include_router(api_router)
+# Reference to all the microservice routes including special routes like info and health
+app.include_router(api_router)
 
-  # Configure API versioning by setting prefix format for all APIs
-  fast_app = VersionedFastAPI(fast_app, version_format="{major}", prefix_format="/api/v{major}")
-
-  # Configure common event handlers for service startup and shutdown to handler special use cases
-  # fast_app.add_event_handler("startup", start_app_handler(fast_app))
-  # fast_app.add_event_handler("shutdown", shutdown_app_handler(fast_app))
-
-  return fast_app
-
-app = get_app()
+# Configure API versioning by setting prefix format for all APIs
+app = VersionedFastAPI(app, version_format="{major}", prefix_format="/api/v{major}")
 
 # A function that should be run before the application starts.
 # Your application won't start receiving requests
